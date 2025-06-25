1225e86c67b2d6fd6eb9acebeca32ce8141e31ce
sujeet kumar <sujeetbhardwaj9580@gmail.com>
2025-06-25 17:52:21 +0530

    Variables And Datatypes

---
+++ JavaScript/Variables And DataType/Var.js
@@ -0,0 +1,99 @@
+// first phase
+// username="sujeet";
+// console.log(username);
+// a=5;
+// b=10;
+// console.log(a+b);
+// const pi=3.14;
+// console.log(pi);
+// // pi=4.24; 
+// // gives error 
+// let x;
+// console.log(x);
+
+
+// second phase 
+
+// isStudent=true;
+// console.log(typeof(isStudent));
+
+// let first=null;
+// console.log(typeof(first));
+// console.log(typeof(NaN));
+// let colors = ["red", "blue", "green"]
+// console.log(colors);
+// const person={
+//     name:"sujeet",
+//    age:21
+// }
+// console.log(person);
+
+
+// third phase 
+
+// type conversion 
+
+
+// let num="123"
+
+// let num1=Number(num);
+// console.log(num1);
+// let num2=245
+// let num3=String(num2);
+// console.log(num3);
+// console.log(typeof num3)
+
+// console.log("5" + 3);
+
+// console.log("5" - 3);
+
+
+// fourth Phase 
+
+let r=1;
+console.log(typeof 1)
+console.log(typeof NaN)
+let a = undefined;
+let b = null;
+let c = [1, 2, 3];
+let d = {name: "Tom"};
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+
+let x = "true"
+console.log(typeof x);
+
+
+// var num = 10;
+// {
+//     var num = 20;
+// }
+// console.log(num);
+
+let num = 10;
+{
+    let num = 20;
+    console.log(num);
+}
+console.log(num);
+
+const per={
+    name:"sujeet",
+    roll:123
+}
+console.log(per.roll)
+per.roll=111
+console.log(per.roll)
+
+
+const user = {
+  name: "Sujeet",
+  age: 25
+};
+user.age = 26;
+user.email = "sujeet@example.com"; 
+console.log(user)
+
